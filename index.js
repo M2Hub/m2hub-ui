10bdbf9133f6ae5bc65fce86cc3d6261d7ae3313
Mr. India <mr.india.dev@gmail.com>
2018-05-08 16:42:00 +0530

    Added index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var APPYBE_BASE_URL = "https://mavenhub-server.herokuapp.com/";
+var APPYCDN_BASE_URL = "https://appyfy.github.io/cdn/WebContent";
+var APPYUI_BASE_URL = "https://appyfy.github.io/ui/WebContent";
+var APPYSTR_BASE_URL = "/metadata";
+
+injectJS(APPYUI_BASE_URL + "/index.js");

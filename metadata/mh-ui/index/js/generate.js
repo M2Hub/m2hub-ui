de49eca9b4af3cdb18b95f161e3d0bf798281111
Mr. India <mr.india.dev@gmail.com>
2018-05-29 08:14:05 +0000

    Added Archetype Generator

---
+++ metadata/mh-ui/index/js/generate.js
@@ -0,0 +1,27 @@
+function generate(actionParams, row) {
+    
+    var params = row.params;
+
+    var request = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://mavenhub-server.herokuapp.com/mvn/-B/archetype/generate",
+      "method": "POST",
+      "headers": {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      "data": {
+        "archetypeGroupId": "org.apache.maven.archetypes",
+        "archetypeArtifactId": params.archetypeArtifactId,
+        "archetypeVersion": "1.3",
+        "groupId": params.groupId,
+        "artifactId": params.artifactId,
+        "version": "1.0.0"
+      }
+    };
+    
+    $.ajax(request).done(function (response) {
+        console.log(response);
+        alert('Project : ' + params.artifactId + ' generated successfully');
+    });
+}

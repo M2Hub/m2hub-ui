4412e882172a65b693c8a2990171a5e2f4574b38
Mr. India <mr.india.dev@gmail.com>
2018-09-06 13:32:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var APPYBE_BASE_URL = "https://mavenhub-server.herokuapp.com/";
+var APPYBE_BASE_URL = "https://m2hub-server.herokuapp.com/";
 var APPYCDN_BASE_URL = "https://appyfy.github.io/cdn/WebContent";
 var APPYUI_BASE_URL = "https://appyfy.github.io/ui/WebContent";
 var APPYSTR_BASE_URL = "./metadata";
